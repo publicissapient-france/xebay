a04104b6a144ec5963f7c25c218f948d67d8c36a
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-12-19 04:04:20 +0100

    transfert increment logic to BidOffer

--- src/main/java/bid/BidOffer.java
+++ src/main/java/bid/BidOffer.java
@@ -1,5 +1,7 @@
 package bid;
 
+import static java.lang.String.format;
+
 class BidOffer {
     private final Item item;
 
@@ -19,16 +21,30 @@ class BidOffer {
         return currentValue;
     }
 
-    BidOffer increment(double increment, User user) {
-        this.currentValue += increment;
-        this.user = user;
-        return this;
-    }
-
     void resolve() {
         if (user != null) {
             item.updateValue(currentValue);
             user.buy(item);
         }
+    }
+
+    BidOffer increment(String name, double value, double increment, User user) throws BidException {
+        if (!item.getName().equals(name)) {
+            throw new BidException(format("current item to bid is not \"%s\"", name));
+        }
+        if (currentValue != value) {
+            throw new BidException(format("value for \"%s\" is not %s but %s", item.getName(), Double.toString(value), Double.toString(currentValue)));
+        }
+        if (currentValue / 10 > increment) {
+            throw new BidException(format("increment %s is less than ten percent of initial value %s of item \"%s\"", Double.toString(increment), Double.toString(item.getValue()), item.getName()));
+        }
+
+        return increment(increment, user);
+    }
+
+    private BidOffer increment(double increment, User user) {
+        this.currentValue += increment;
+        this.user = user;
+        return this;
     }
 }
--- src/main/java/bid/BidServer.java
+++ src/main/java/bid/BidServer.java
@@ -3,7 +3,6 @@ package bid;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 
-import static java.lang.String.format;
 import static java.util.Arrays.asList;
 
 class BidServer {
@@ -33,17 +32,7 @@ class BidServer {
     }
 
     BidOffer bid(String key, String name, double value, double increment) throws BidException {
-        User user = users.findByKey(key);
-        if (!current.getItem().getName().equals(name)) {
-            throw new BidException(format("current item to bid is not \"%s\"", name));
-        }
-        if (current.getCurrentValue() != value) {
-            throw new BidException(format("value for \"%s\" is not %s but %s", current.getItem().getName(), Double.toString(value), Double.toString(current.getCurrentValue())));
-        }
-        if (current.getCurrentValue() / 10 > increment) {
-            throw new BidException(format("increment %s is less than ten percent of initial value %s of item \"%s\"", Double.toString(increment), Double.toString(current.getItem().getValue()), current.getItem().getName()));
-        }
-        return current.increment(increment, user);
+        return current.increment(name, value, increment, users.findByKey(key));
     }
 
     void tick() {
