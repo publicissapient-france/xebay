8bf16b13e26b4b3e3d299731e886fc874a59246e
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-01-22 21:42:13 +0100

    load items declared into file "items"

--- src/main/java/fr/xebia/xebay/BidServer.java
+++ src/main/java/fr/xebia/xebay/BidServer.java
@@ -1,15 +1,20 @@
 package fr.xebia.xebay;
 
 import fr.xebia.xebay.domain.BidEngine;
+import fr.xebia.xebay.domain.Item;
 import fr.xebia.xebay.domain.Items;
 
 public enum BidServer {
     BID_SERVER;
 
-    private BidEngine bidEngine;
+    private final BidEngine bidEngine;
 
     BidServer() {
-        this.bidEngine = new BidEngine(Items.load("items").get());
+        if (System.getProperty("xebay.test") != null) {
+            bidEngine = new BidEngine(new Items(new Item("an item", 4.3)), () -> false);
+            return;
+        }
+        bidEngine = new BidEngine(Items.load("items").get());
     }
 
     public BidEngine bidEngine() {
--- src/main/java/fr/xebia/xebay/api/rest/BidEngineResource.java
+++ src/main/java/fr/xebia/xebay/api/rest/BidEngineResource.java
@@ -3,7 +3,8 @@ package fr.xebia.xebay.api.rest;
 import fr.xebia.xebay.api.rest.dto.BidOfferInfo;
 import fr.xebia.xebay.api.rest.dto.BidParam;
 import fr.xebia.xebay.api.rest.security.UserAuthorization;
-import fr.xebia.xebay.domain.*;
+import fr.xebia.xebay.domain.BidEngine;
+import fr.xebia.xebay.domain.User;
 
 import javax.annotation.security.PermitAll;
 import javax.inject.Singleton;
@@ -11,20 +12,18 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
-import java.util.logging.Logger;
+
+import static fr.xebia.xebay.BidServer.BID_SERVER;
 
 @Singleton
 @Path("/bidEngine")
 @PermitAll
 @Produces(MediaType.APPLICATION_JSON)
 public class BidEngineResource {
-    private static final Logger log = Logger.getLogger("BidEngineResource");
-
     private final BidEngine bidEngine;
 
-
     public BidEngineResource() {
-        bidEngine = new BidEngine(new Items(new Item("an item", 4.3)));
+        bidEngine = BID_SERVER.bidEngine();
     }
 
     @GET
@@ -33,26 +32,21 @@ public class BidEngineResource {
         return BidOfferInfo.newBidOfferInfo(bidEngine.currentBidOffer());
     }
 
-
     @POST
     @Path("/bid")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @UserAuthorization
-    public BidOfferInfo bid(@FormParam("name") String name, @FormParam("value") Double curValue, @FormParam("increment") Double increment, @Context SecurityContext securityContext){
-        User user = (User)securityContext.getUserPrincipal();
+    public BidOfferInfo bid(@FormParam("name") String name, @FormParam("value") Double curValue, @FormParam("increment") Double increment, @Context SecurityContext securityContext) {
+        User user = (User) securityContext.getUserPrincipal();
         return BidOfferInfo.newBidOfferInfo(bidEngine.bid(user, name, curValue, increment));
     }
-
 
     @POST
     @Path("/bid")
     @Consumes(MediaType.APPLICATION_JSON)
     @UserAuthorization
-    public BidOfferInfo bid(
-                        BidParam bidParam,
-                        @Context SecurityContext securityContext){
-        User user = (User)securityContext.getUserPrincipal();
+    public BidOfferInfo bid(BidParam bidParam, @Context SecurityContext securityContext) {
+        User user = (User) securityContext.getUserPrincipal();
         return BidOfferInfo.newBidOfferInfo(bidEngine.bid(user, bidParam.getItemName(), bidParam.getCurValue(), bidParam.getIncrement()));
     }
-
 }
