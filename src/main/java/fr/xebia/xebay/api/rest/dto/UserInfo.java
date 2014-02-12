d4ccd4153d710f2cccfe652406c7897ab5d76f28
scherrda <dahlia.scherr@gmail.com>
2014-02-12 23:38:11 +0100

    UserInfo has no info about items

--- src/main/java/fr/xebia/xebay/api/rest/dto/UserInfo.java
+++ src/main/java/fr/xebia/xebay/api/rest/dto/UserInfo.java
@@ -1,24 +1,19 @@
 package fr.xebia.xebay.api.rest.dto;
 
 
-import fr.xebia.xebay.domain.Item;
 import fr.xebia.xebay.domain.User;
-
-import java.util.Set;
 
 public class UserInfo {
     private String email;
     private double balance;
-    private Set<Item> items;
 
-    public UserInfo(String email, double balance, Set<Item> items) {
+    public UserInfo(String email, double balance) {
         this.email = email;
         this.balance = balance;
-        this.items = items;
     }
 
     public static UserInfo newUserInfo(User user) {
-        return new UserInfo(user.getEmail(), user.getBalance(), user.getItems());
+        return new UserInfo(user.getEmail(), user.getBalance());
     }
 
 
@@ -43,7 +38,6 @@ public class UserInfo {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Email ").append(email);
         stringBuilder.append("Balance ").append(balance);
-        stringBuilder.append("Items ").append(items.stream().toString());
         return stringBuilder.toString();
     }
 }
