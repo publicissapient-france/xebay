3effd2450908ce664c95f6f8bc89c0139f57abc7
scherrda <dahlia.scherr@gmail.com>
2014-02-14 12:59:21 +0100

    api offer

--- src/test/java/fr/xebia/xebay/api/rest/BidEngineResourceTest.java
+++ src/test/java/fr/xebia/xebay/api/rest/BidEngineResourceTest.java
@@ -1,6 +1,9 @@
 package fr.xebia.xebay.api.rest;
 
+import fr.xebia.xebay.api.rest.dto.ItemOffer;
 import fr.xebia.xebay.domain.BidEngine;
+import fr.xebia.xebay.domain.User;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -9,16 +12,24 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.SecurityContext;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BidEngineResourceTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    @Mock
-    private BidEngine bidEngine;
+    @Mock private BidEngine bidEngine;
+    private User user;
+    private BidEngineResource bidEngineResource;
+
+    @Before
+    public void init() {
+        bidEngineResource = new BidEngineResource(bidEngine);
+        user = new User("key1", "user1");
+    }
 
     @Test
     public void should_return_404_if_no_bid_offer_can_be_retrieved() {
@@ -28,4 +39,29 @@ public class BidEngineResourceTest {
 
         new BidEngineResource(bidEngine).currentBidOffer();
     }
+
+    @Test
+    public void should_call_bidEngine_offer_when_user_offering_item() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+
+        when(securityContext.getUserPrincipal()).thenReturn(user);
+
+        ItemOffer itemOffer = new ItemOffer("an item", 10.0);
+
+        bidEngineResource.offer(itemOffer, securityContext);
+
+        verify(bidEngine, times(1)).offer(user, "an item", 10.0);
+    }
+    //TODO test should check NotFoundException @Test(expected = BidException.class)
+    public void should_throw_notFoundException_when_offering_unknown_item() throws Exception {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getUserPrincipal()).thenReturn(user);
+
+        String itemName = "unknown item";
+
+//        doThrow(new BidException("unknown item not found")).when(bidEngine).offer(user, itemName, 10.0);
+
+//        bidEngineResource.offer(new ItemOffer(itemName, 10.0), securityContext);
+    }
+
 }
