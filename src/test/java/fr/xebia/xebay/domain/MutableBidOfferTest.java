3fde66ab7c120bf0f42baf5a9bc3f72aa1779c96
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-02-18 01:09:24 +0100

    when create MutableBidOffer currentValue should be initialValue

---
+++ src/test/java/fr/xebia/xebay/domain/MutableBidOfferTest.java
@@ -0,0 +1,16 @@
+package fr.xebia.xebay.domain;
+
+import org.junit.Test;
+
+import static fr.xebia.xebay.domain.BidEngine.DEFAULT_TIME_TO_LIVE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MutableBidOfferTest {
+    @Test
+    public void should_have_same_value_for_current_and_intial_value_at_startup() {
+        MutableBidOffer mutableBidOffer = new MutableBidOffer(null, 4.3, DEFAULT_TIME_TO_LIVE);
+
+        assertThat(mutableBidOffer.getInitialValue()).isEqualTo(4.3);
+        assertThat(mutableBidOffer.getCurrentValue()).isEqualTo(4.3);
+    }
+}
